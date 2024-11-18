297b8d8213d262e70859b82e9e604739db39a8dd
AnujaLd <isanujadewnim@gmail.com>
2024-11-18 17:00:12 +0530

    Create linkedlist.py

---
+++ linkedlist.py
@@ -0,0 +1,11 @@
+
+class node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class sll:
+    def __init__(self):
+        self.head = None
+        self.tail = None
